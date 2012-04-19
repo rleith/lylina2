f943e170cea3811dc51444cb67001f8f2a6cf24e
Robert Leith <rleith@gmail.com>
2012-04-18 20:45:15 -0400

    Correct database structure in delta script

--- database/004-add_feed_updating_col.delta.sql
+++ database/004-add_feed_updating_col.delta.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `lylina_feeds` ADD `updating` TINYINT( 1 ) NOT NULL DEFAULT '0'
+ALTER TABLE `lylina_feeds` ADD `updating` TINYINT( 1 ) NOT NULL DEFAULT '0';
+ALTER TABLE `lylina_feeds` CHANGE `lastmod` `lastmod` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
