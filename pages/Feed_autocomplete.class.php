daa0ee4d411886443fa17a921ad0ab6c05c35df6
Robert Leith <rleith@gmail.com>
2012-05-05 13:24:32 -0400

    Improved feed autocomplete
    Autocomplete will now:
     - Only show feeds you have not already subscribed to
     - Only show feeds that more than two people have subscribed to avoid showing private feeds
     - Order the suggested feeds by number of subscribers
     - Limit the number of suggestions shown on the page

--- pages/Feed_autocomplete.class.php
+++ pages/Feed_autocomplete.class.php
@@ -4,24 +4,39 @@
 // Copyright (C) 2004-2005 Panayotis Vryonis
 // Copyright (C) 2005 Andreas Gohr
 // Copyright (C) 2006-2010 Eric Harmon
-// Copyright (C) 2011 Robert Leith
+// Copyright (C) 2011-2012 Robert Leith
 
 // AJAX JSON data for autocompleting feeds
 class Feed_autocomplete {
-	private $db;
-	function __construct() {
-		global $db;
-		$this->db = $db;
-	}
+    private $db;
+    private $auth;
+    function __construct() {
+        global $db;
+        global $auth;
+        $this->db = $db;
+        $this->auth = $auth;
+    }
 
-	function render() {
-        if(!isset($_REQUEST['term'])) {
+    function render() {
+        if(!isset($_REQUEST['term']) || !$this->auth->check()) {
             return;
         }
 
         $term = isset($_REQUEST['term']) ? $_REQUEST['term'] : NULL;
 
-        $feeds = $this->db->GetAll('SELECT lylina_feeds.name, lylina_feeds.url FROM lylina_feeds WHERE lylina_feeds.name LIKE ? OR lylina_feeds.url LIKE ?', array("%$term%", "%$term%"));
+        $feeds = $this->db->GetAll('
+                    SELECT lylina_feeds.name,
+                           lylina_feeds.url,
+                           (select count(*) from lylina_userfeeds where lylina_userfeeds.feed_id = lylina_feeds.id) AS subscribers
+                    FROM lylina_feeds
+                    WHERE
+                        lylina_feeds.id NOT IN (select feed_id from lylina_userfeeds where user_id = ?)
+                        AND
+                        (select count(*) from lylina_userfeeds where lylina_userfeeds.feed_id = lylina_feeds.id) > 2
+                        AND
+                        (lylina_feeds.name LIKE ? OR lylina_feeds.url LIKE ?)
+                    ORDER BY subscribers
+                    LIMIT 10', array($this->auth->getUserId(), "%$term%", "%$term%"));
 
         $feed_array = array();
 
@@ -35,5 +50,5 @@ class Feed_autocomplete {
 		$render = new Render();
 		$render->assign('json_data', json_encode($feed_array));
 		$render->display('json_data.tpl');
-	}
+    }
 }
